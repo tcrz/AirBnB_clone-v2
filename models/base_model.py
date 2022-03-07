c335a2cc760101761c04142cb2d3fadab98375b7
Tonycrz <tonybaidoo0@gmail.com>
2022-03-07 13:27:38 +0000

    sqlalchemy + new methods

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,26 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, DEFAULT=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, DEFAULT=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
+            # from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +50,21 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        for k, v in list(dictionary.items()):
+            # remove the key _sa_instance_state from the dictionary
+            # returned by this method only if this key exists
+            if k == '_sa_instance_state':
+                dictionary.pop(k)
+            else:
+                pass
         return dictionary
+
+    def delete(self):
+        """deletes the current instance from the storage"""
+        from models import storage
+        this_obj = self.__class__.__name__ + '.' + self.id
+        stored = storage.all()
+        for k, v in list(stored.items()):
+            if k == this_obj:
+                stored.pop(k)
+        # storage.save()
