300f3ac9e1f15c9fedf1753ba0a45818ce80c629
NathyG2524 <Nathygirma2524@gmail.com>
2022-03-09 03:19:22 -0800

    Revert "DB storage"
    This reverts commit d5ec70543618087c4468c297f644ba6bb23b96aa.

--- models/city.py
+++ models/city.py
@@ -2,7 +2,6 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,4 +9,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade="all, delete-orphan", backref="cities")
+    places = relationship("Place", cascade="all, delete-orphan" backref="cities")
--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
