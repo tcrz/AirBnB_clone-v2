06b3530680e73ef650ac8f1495b6ebdc7a91dcf3
Tonycrz <tonybaidoo0@gmail.com>
2022-03-23 19:43:40 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,12 +6,12 @@ from fabric.api import local
 from datetime import datetime
 import os
 
-filename = "web_static" + datetime.now().replace(microsecond=0)\
+filename = "web_static_" + datetime.now().replace(microsecond=0)\
 .strftime("%Y%m%d%H%M%S") + '.tgz'
 
 
 def do_pack():
-    print("Packing web_static to versions/", filename)
+    print("Packing web_static to versions/{}".format(filename))
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     local("tar -czvf versions/{} web_static".format(filename))
