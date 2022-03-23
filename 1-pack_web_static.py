8bf3c45f8050466ad4d6c0cb286ece484fed6b30
Tonycrz <tonybaidoo0@gmail.com>
2022-03-23 19:49:16 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import os
 
 filename = "web_static_" + datetime.now().replace(microsecond=0)\
-.strftime("%Y%m%d%H%M%S") + '.tgz'
+            .strftime("%Y%m%d%H%M%S") + '.tgz'
 
 
 def do_pack():
@@ -15,5 +15,5 @@ def do_pack():
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     local("tar -czvf versions/{} web_static".format(filename))
-    print("web_static packed: versions/{} -> {}Bytes"\
-          .format(filename, os.path.getsize("versions/{}".format(filename))))
+    print("web_static packed: versions/{} -> {}Bytes".format(
+        filename, os.path.getsize("versions/{}".format(filename))))
