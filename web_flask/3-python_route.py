e87eaab47d08c78a14d8dd09633c1656ea35d6b9
Tonycrz <tonybaidoo0@gmail.com>
2022-04-07 21:54:20 +0000

    docstring_update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,6 +6,7 @@ script that starts a Flask web application:
 - /: display “Hello HBNB!”
 - /hbnb: display "HBNB"
 - /c/<text>: display “C ”, followed by the value of the text variable
+- python/<text>: display “Python ”, followed by the value of the text variable
 - use the option strict_slashes=False in your route definition
 """
 
@@ -37,7 +38,7 @@ def c_route(text):
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text):
-    """/python/(<text>): display “Python ”, followed by the value of the
+    """/python/<text>: display “Python ”, followed by the value of the
 text variable (replace underscore _ symbols with a space )"""
     return "Python " + text.replace("_", " ")
 
