1a549c6a30de9a57c649d095adec56a0cb554ae8
Tonycrz <tonybaidoo0@gmail.com>
2022-04-08 20:13:38 +0000

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -9,5 +9,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
-                                   overlaps="amenities",
+                                   overlaps="amenities", viewonly=False,
                                    back_populates="amenities")
