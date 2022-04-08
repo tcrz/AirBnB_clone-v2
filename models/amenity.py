c82c75ac402b9cec9635adbacead0dc17c84a657
Tonycrz <tonybaidoo0@gmail.com>
2022-04-08 20:03:51 +0000

    update

--- models/amenity.py
+++ models/amenity.py
@@ -8,4 +8,5 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   overlaps="amenities")
