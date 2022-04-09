3b28afedce22f616a39b82b78e9471034422fbc8
Tonycrz <tonybaidoo0@gmail.com>
2022-04-09 11:27:08 +0000

    init_update

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            # from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            # storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+        # from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        # storage.new(self)
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(self.created_at.
+                                                     isoformat(),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['updated_at'] = datetime.strptime(self.created_at.
+                                                     isoformat(),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if "__class__" in kwargs.keys():
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
